964b29080ba4c2e4b26ba048b25c4457a1d789a1
anup.bhandegaonkar <anup.bhandegaonkar@gmail.com>
2018-02-18 11:32:53 +0530

    Initial Commit

---
+++ telecom-service/src/test/java/corp/telecomservice/TelecomServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package corp.telecomservice;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TelecomServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		fail();
+	}
+
+}
